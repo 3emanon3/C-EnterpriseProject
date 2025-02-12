924b5c9b51771cc21ff08f46ce7e57edef1709b0
3emanon3 <tanjuinwei1009@gmail.com>
2025-02-12 16:37:32 +0800

    done API testing

--- databaseConnection.php
+++ databaseConnection.php
@@ -5,20 +5,5 @@ $user = 'root';
 $pass = '0000';
 $charset = 'utf8mb4';
 
-<<<<<<< HEAD
 $dsn = mysqli_connect($host, $user, $pass, $db);
-=======
-$dsn = mysqli_connect(
-                      $host,
-                      $user,
-                      $pass,
-                      $db
-);
-
-if ($dsn) {
-    echo "Database connected successfully";
-} else {
-    echo "Database connection failed";
-}
->>>>>>> 60cae8894ec77caf56121757ddf9049443b62e82
 ?>
