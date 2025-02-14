d68b1f21aebb0ab0b891e11fd8748a53283e002c
3emanon3 <tanjuinwei1009@gmail.com>
2025-02-14 12:40:26 +0800

    delete unused files

--- databaseConnection.php
+++ databaseConnection.php
@@ -2,16 +2,9 @@
 $host = 'localhost';
 $db   = 'entreprise_test';
 $user = 'root';
-$pass = '';
+$pass = '0000';
 $charset = 'utf8mb4';
 
 // Create connection
 $dsn = mysqli_connect($host, $user, $pass, $db);
-
-// Check connection
-if (!$dsn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
 ?>
